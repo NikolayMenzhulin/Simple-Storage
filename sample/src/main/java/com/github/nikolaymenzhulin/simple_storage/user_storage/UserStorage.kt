b22f1688ae5299aa2b3d0cd3c13a0014fcf5d786
Nikolay Menzhulin <NikolayMenzhulin@gmail.com>
2021-04-04 23:03:35 +0300

    Added a sample

---
+++ sample/src/main/java/com/github/nikolaymenzhulin/simple_storage/user_storage/UserStorage.kt
@@ -0,0 +1,10 @@
+package com.github.nikolaymenzhulin.simple_storage.user_storage
+
+import com.github.nikolaymenzhulin.simple_storage.base_file_storage.BaseStringFileStorage
+import com.github.nikolaymenzhulin.simple_storage.file_name_generator.SimpleFileNameGenerator
+import com.github.nikolaymenzhulin.simple_storage.file_processor.FileProcessor
+
+class UserStorage(cacheDirPath: String) : BaseStringFileStorage(
+        fileProcessor = FileProcessor(cacheDirPath = cacheDirPath, cacheDirName = "users_cache", maxFilesCount = 5),
+        fileNameGenerator = SimpleFileNameGenerator()
+)
